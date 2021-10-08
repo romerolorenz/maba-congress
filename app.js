ffc9f28b5a7f21a66e05c8947247661736b9fe8f
mlromero <marco@ubx.ph>
2021-10-08 10:37:18 +0800

    added winston

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 'use strict';
 
+require('dotenv').config();
+
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
+const helmet = require('helmet');
+const {logger} = require('./api/utilities/logger.js');
+
+app.use(helmet());
+
 module.exports = app; // for testing
 
 var config = {
@@ -17,7 +24,5 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   var port = process.env.PORT || 10010;
   app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+  logger.log('info', 'API Started...')
 });
